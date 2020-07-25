c97b8fbabf1e96b4a2f30adb41e1c0bbfa7f5b42
zhaocheng <zhaocheng@westwin.com>
2020-07-25 16:32:33 +0800

    8088

--- deploy-server.sh
+++ deploy-server.sh
@@ -17,6 +17,6 @@ fi
 # 	sudo docker rmi -f $imageId
 # fi
 
-sudo docker run -d -p 8081:80 --restart=always 542153354/identityserver4:v1.0 /bin/sh 
+sudo docker run -d -p 8088:80 --restart=always 542153354/identityserver4:v1.0 /bin/sh 
 
 exit
