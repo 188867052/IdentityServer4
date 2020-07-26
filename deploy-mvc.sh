8078ec8a86afdc8c1cf08647929b3d6cd45f97bd
188867052 <47692838+188867052@users.noreply.github.com>
2020-07-26 11:45:45 +0800

    Update deploy-mvc.sh

--- deploy-mvc.sh
+++ deploy-mvc.sh
@@ -11,4 +11,45 @@ then
 fi
 
 sudo docker run -d -p 8089:80 --restart=always 542153354/mvcclient:v1.0 /bin/sh 
+#-------------------------------------------------------------------------------
+containerId="`sudo docker ps | grep "8092->80" | awk  '{print $1}'`"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+sudo docker run -d -p 8092:80 --restart=always 542153354/mvcclient:v1.0 /bin/sh 
+#-------------------------------------------------------------------------------
+containerId="`sudo docker ps | grep "8093->80" | awk  '{print $1}'`"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+sudo docker run -d -p 8093:80 --restart=always 542153354/mvcclient:v1.0 /bin/sh 
+#-------------------------------------------------------------------------------
+containerId="`sudo docker ps | grep "8094->80" | awk  '{print $1}'`"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+sudo docker run -d -p 8094:80 --restart=always 542153354/mvcclient:v1.0 /bin/sh 
+#-------------------------------------------------------------------------------
+containerId="`sudo docker ps | grep "8095->80" | awk  '{print $1}'`"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+sudo docker run -d -p 8095:80 --restart=always 542153354/mvcclient:v1.0 /bin/sh 
+
 exit
